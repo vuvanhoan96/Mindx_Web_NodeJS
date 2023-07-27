c9c3d83be782801c559d7ca4b81eb536b3bdbe78
vuvanhoan96 <vuhoan9603@gmail.com>
2023-07-27 21:55:14 +0700

    lesson-02-expressjs

---
+++ lesson-02-expressjs/src/app.js
@@ -0,0 +1,41 @@
+import express from "express"
+
+const PORT = 8000;
+const app = express();
+
+app.use(express.json()); // middlewares
+/*
+    Đây là 3 cách truyền dữ liệu của người dùng gửi tới Server nhận được: 
+
+        req.params
+        req.query
+        req.body
+
+*/
+
+// req.params
+
+app.get('/products/:id', (req, res) => {
+
+    res.send(`
+        <h1>Hi everyone</h1>
+        <p>Day la method ${req.method} tai url: ${req.url}</p>
+        <p>Day la san pham co id: ${req.params.id}</p>
+    `)
+})
+
+// req.body
+
+app.post("/auth/signin", (req, res) => {
+
+    console.log(req.body);
+})
+
+// req.query
+
+app.get("/products", (req, res) => {
+    console.log(req.query);
+})
+app.listen(PORT, () => {
+    console.log("Server is running at PORT: 8000");
+})
